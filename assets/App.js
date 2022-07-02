d7abc26cd75b106d12a73ad4748f2bccf58fa5ef
melikatfi <87155734+melikatfi@users.noreply.github.com>
2022-07-02 14:55:42 +0430

    buy session done

--- assets/App.js
+++ assets/App.js
@@ -61,3 +61,29 @@ $(document).on('change', '.checking', function () {
         $(this).closest('.color').removeClass('active');
     }
 });
+
+$(document).ready(function() {
+// Swiper: Slider
+    new Swiper('.swiper-container', {
+        loop: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 3,
+        paginationClickable: true,
+        spaceBetween: 20,
+        breakpoints: {
+            1920: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1028: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
+});
